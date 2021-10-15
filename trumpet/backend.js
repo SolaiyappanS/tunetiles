344a00812d10a4f3a9adec1bd4e8c86effd7fea0
Solai <solaiyappatce@gmail.com>
2021-10-15 11:57:01 +0530

    trumpet added

---
+++ trumpet/backend.js
@@ -0,0 +1,35 @@
+var keys = ['E','R','T','Y','U','D','F','G','H','J']
+
+function playNote(code){
+    if(keys.indexOf(code) != -1){
+        var audio = document.getElementById("key"+code);
+        document.getElementById("button"+code).style.backgroundColor = '#be901c';
+        document.getElementById("button"+code).style.boxShadow = '0 10px 30px rgb(1 1 1 / 0.2)';
+        audio.currentTime = 0;
+        audio.play();
+    }
+}
+
+document.addEventListener('keydown', playKey);
+document.addEventListener('keyup', normal);
+
+function playKey(e) {
+    playNote((e.key).toUpperCase());
+}
+
+function normal(){
+    initialSetup();
+    var audio;
+    for(var i=0; i<keys.length; i++){
+        audio = document.getElementById("key"+keys[i]);
+        audio.currentTime = 0;
+        audio.pause();
+    }
+}
+
+function initialSetup(){
+    for(var i=0; i<keys.length; i++){
+        document.getElementById("button"+keys[i]).style.backgroundColor = '#daa520';
+        document.getElementById("button"+keys[i]).style.boxShadow = 'none';
+    }
+}
